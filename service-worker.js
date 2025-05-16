b1a09f60cecdb92b9d41036836abfe5f142dd8de
SAAD <96654865+0Sa-ad0@users.noreply.github.com>
2025-05-16 23:03:16 +0600

    ok

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,38 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
-workbox.precaching.precacheAndRoute(self.__WB_MANIFEST || []);
-workbox.routing.registerRoute(
-    ({ request }) => request.destination === 'image',
-    new workbox.strategies.CacheFirst({
-        // new workbox.strategies.NetworkFirst({
-        cacheName: 'images',
-        plugins: [
-            new workbox.expiration.ExpirationPlugin({
-                maxEntries: 50,
-                maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-            }),
-        ],
-    })
-);
+// importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
+// workbox.precaching.precacheAndRoute(self.__WB_MANIFEST || []);
+// workbox.routing.registerRoute(
+//     ({ request }) => request.destination === 'image',
+//     new workbox.strategies.CacheFirst({
+//         // new workbox.strategies.NetworkFirst({
+//         cacheName: 'images',
+//         plugins: [
+//             new workbox.expiration.ExpirationPlugin({
+//                 maxEntries: 50,
+//                 maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+//             }),
+//         ],
+//     })
+// );
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open('forex-clock-cache').then(cache => {
+            return cache.addAll([
+                './',
+                './index.html',
+                './style.css',
+                './script.js',
+                './manifest.json'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(resp => {
+            return resp || fetch(event.request);
+        })
+    );
+});
+F
